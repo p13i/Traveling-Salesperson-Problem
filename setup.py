f844cb67f71bd585e6d3814bc82ab8e0d285534e
Pramod Kotipalli <pramod.kotipalli@gmail.com>
2018-02-22 00:16:02 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='traveling-salesman-problem',
+    version=VERSION,
+    description="Traveling Salesman Problem - Solutions in Python",
+    long_description=read('README.md'),
+    author='Pramod Kotipalli - School of Interactive Computing - Georgia Institute of Technology',
+    author_email='pramodk@gatech.edu',
+    url='https://github.com/p13i/Traveling-Salesman-Problem',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=True,
+)
